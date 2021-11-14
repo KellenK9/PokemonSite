04cbdded9aa7924f3dec20ee689cdfdfa1c3ba97
KellenK9 <55595944+KellenK9@users.noreply.github.com>
2021-11-14 13:12:19 -0600

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -21,30 +21,30 @@ def comparison():
     poke2 = str(request.form['text2']).lower()
 
     for i in range(len(reader)):
-        if str(reader[i][4]).lower() == poke1:
+        if str(reader[i][2]).lower() == poke1:
             num1 = i
-        if str(reader[i][4]).lower() == poke2:
+        if str(reader[i][2]).lower() == poke2:
             num2 = i
 
-    attack1 = reader[num1][0]
-    total1 = reader[num1][1]
-    defense1 = reader[num1][2]
-    hp1 = reader[num1][3]
-    spattack1 = reader[num1][5]
-    spdefense1 = reader[num1][6]
-    speed1 = reader[num1][7]
-    type11 = reader[num1][8]
-    type12 = reader[num1][9]
+    attack1 = reader[num1][14]
+    defense1 = reader[num1][15]
+    hp1 = reader[num1][13]
+    spattack1 = reader[num1][16]
+    spdefense1 = reader[num1][17]
+    speed1 = reader[num1][18]
+    type11 = reader[num1][4]
+    type12 = reader[num1][5]
+    total1 = hp1 + attack1 + defense1 + spattack1 + spdefense1 + speed1
 
-    attack2 = reader[num2][0]
-    total2 = reader[num2][1]
-    defense2 = reader[num2][2]
-    hp2 = reader[num2][3]
-    spattack2 = reader[num2][5]
-    spdefense2 = reader[num2][6]
-    speed2 = reader[num2][7]
-    type21 = reader[num2][8]
-    type22 = reader[num2][9]
+    attack2 = reader[num2][14]
+    defense2 = reader[num2][15]
+    hp2 = reader[num2][13]
+    spattack2 = reader[num2][16]
+    spdefense2 = reader[num2][17]
+    speed2 = reader[num2][18]
+    type21 = reader[num2][4]
+    type22 = reader[num2][5]
+    total2 = hp2 + attack2 + defense2 + spattack2 + spdefense2 + speed2
 
     return render_template('display.html', pokemon1 = poke1, pokemon2 = poke2, attack1 = attack1, attack2 = attack2, total1 = total1, total2 = total2, defense1 = defense1, defense2 = defense2, hp1 = hp1, hp2 = hp2, spattack1 = spattack1, spattack2 = spattack2, spdefense1 = spdefense1, spdefense2 = spdefense2, speed1 = speed1, speed2 = speed2, type11 = type11, type12 = type12, type21 = type21, type22 = type22)
 
